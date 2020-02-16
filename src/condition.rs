74ccc10205a015b43bfc4e8bd45da1b8ca484ea0
Josh Mcguigan <joshmcg88@gmail.com>
2020-02-16 12:41:04 -0800

    added test for eq condition

--- src/condition.rs
+++ src/condition.rs
@@ -2,7 +2,7 @@ use crate::Key;
 
 use std::marker::PhantomData;
 
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub enum ConditionState {
     Satisfied,
     Unsatisfied,
@@ -45,5 +45,74 @@ pub fn eq<E, K, T>(key: K, value: T) -> Eq<E, K,T>
         value,
         state: ConditionState::Unsatisfied,
         marker: PhantomData,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    enum TestEvent {
+        TestEventOne(TestEventOne),
+        TestEventTwo(TestEventTwo),
+    }
+
+    struct TestEventOne {
+        field_a: u32,
+        field_b: u32,
+    }
+
+    struct TestEventTwo {
+        field_a: u32,
+    }
+
+    struct Event1FieldA;
+    struct Event1FieldB;
+    struct Event2FieldA;
+
+    impl Key<TestEvent, u32> for Event1FieldA {
+        fn check(&self, event: &TestEvent) -> Option<u32> {
+            if let TestEvent::TestEventOne(event) = event {
+                return Some(event.field_a)
+            }
+
+            None
+        }
+    }
+
+    impl Key<TestEvent, u32> for Event1FieldB {
+        fn check(&self, event: &TestEvent) -> Option<u32> {
+            if let TestEvent::TestEventOne(event) = event {
+                return Some(event.field_b)
+            }
+
+            None
+        }
+    }
+
+    impl Key<TestEvent, u32> for Event2FieldA {
+        fn check(&self, event: &TestEvent) -> Option<u32> {
+            if let TestEvent::TestEventTwo(event) = event {
+                return Some(event.field_a)
+            }
+
+            None
+        }
+    }
+
+    fn example_event_one() -> TestEvent {
+        TestEvent::TestEventOne(TestEventOne { field_a: 5, field_b: 11 })
+    }
+
+    fn example_event_two() -> TestEvent {
+        TestEvent::TestEventTwo(TestEventTwo { field_a: 1 })
+    }
+
+    #[test]
+    fn equal() {
+        let mut condition = eq(Event1FieldA, 5);
+
+        assert_eq!(ConditionState::Unsatisfied, condition.process_event(&example_event_two()));
+        assert_eq!(ConditionState::Satisfied, condition.process_event(&example_event_one()));
     }
 }
