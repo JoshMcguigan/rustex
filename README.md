1631ee9f539aa38840f1c3a843425c60cee8db9d
Josh Mcguigan <joshmcg88@gmail.com>
2020-02-16 13:47:27 -0800

    update example to show and condition

--- README.md
+++ README.md
@@ -3,15 +3,18 @@
 Easily make assertions against time series data.
 
 ```rust
-pub fn example_expectation() -> impl Expect {
+pub fn example_continuous_expectation() -> impl Expect<Event> {
     verify("My first continuous expect")
         .given(eq(EventTypeOneFieldOne, 10))
         .then(eq(EventTypeTwoFieldTwo, 11))
 }
 
-pub fn example_triggered_expectation() -> impl Expect {
+pub fn example_triggered_expectation() -> impl Expect<Event> {
     verify("My first triggered expect")
-        .given(eq(EventTypeOneFieldOne, 10))
+        .given(
+            eq(EventTypeOneFieldOne, 10)
+            .and(eq(EventTypeOneFieldTwo, 0.46))
+        )
         .when(eq(EventTypeOneFieldTwo, 1.23))
         .then(eq(EventTypeTwoFieldTwo, 11))
 }
--- src/expectations.rs
+++ src/expectations.rs
@@ -1,4 +1,4 @@
-use crate::{verify, eq, Event, Expect, EventTypeOneFieldOne, EventTypeOneFieldTwo, EventTypeTwoFieldTwo};
+use crate::{verify, eq, Condition, Event, Expect, EventTypeOneFieldOne, EventTypeOneFieldTwo, EventTypeTwoFieldTwo};
 
 pub fn example_continuous_expectation() -> impl Expect<Event> {
     verify("My first continuous expect")
@@ -8,7 +8,10 @@ pub fn example_continuous_expectation() -> impl Expect<Event> {
 
 pub fn example_triggered_expectation() -> impl Expect<Event> {
     verify("My first triggered expect")
-        .given(eq(EventTypeOneFieldOne, 10))
+        .given(
+            eq(EventTypeOneFieldOne, 10)
+            .and(eq(EventTypeOneFieldTwo, 0.46))
+        )
         .when(eq(EventTypeOneFieldTwo, 1.23))
         .then(eq(EventTypeTwoFieldTwo, 11))
 }
